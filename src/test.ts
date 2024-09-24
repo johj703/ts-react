e2b8f1db92e23612fb6d4c0c647dfc27d8fb2646
Jo <johj703@nate.com>
2024-09-24 23:32:23 +0900

    add: 페이지네이션에 필요한 타입 추가

--- src/test.ts
+++ src/test.ts
@@ -4,6 +4,15 @@ export type Todo = {
   completed: boolean;
 };
 
+export type Paginate = {
+  first: number;
+  item: number;
+  last: number;
+  next: number | null;
+  pages: number;
+  prev: number | null;
+};
+
 export async function getTodos() {
   const res = await fetch("http://localhost:4000/todos");
   const data: Todo[] = await res.json();
